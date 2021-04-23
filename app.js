e7e5dfcfc3c1ed67be6baf62f518876b4de86d89
Phinahas Philip <phinahasphilip2000@gmail.com>
2021-04-23 23:25:32 +0530

    database connection established

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv/config')
 
-const app = express();  
+const app = express();
+
+
+
+
+//connct to db
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+    console.log("connected");
+})
+
+
 
 //port lisitening 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("started");
 })
